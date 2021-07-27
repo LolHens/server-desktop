1b046e204e75408509d53292120464d05b07ec05
Pierre Kisters <pierrekisters@gmail.com>
2021-07-27 02:04:59 +0200

    app cards

--- frontend/src/main/scala/de/lolhens/serverdesktop/AppComponent.scala
+++ frontend/src/main/scala/de/lolhens/serverdesktop/AppComponent.scala
@@ -4,24 +4,20 @@ import japgolly.scalajs.react.vdom.VdomElement
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{BackendScope, ScalaComponent}
 
-object MainComponent {
-  case class Props()
-
-  private val itemsPerRow = 4
+object AppComponent {
+  case class Props(app: App)
 
   class Backend($: BackendScope[Props, Unit]) {
     def render: VdomElement = {
-      val s = $.state.runNow()
+      val props = $.props.runNow()
       <.div(
-        ^.cls := "container my-5 d-flex flex-row flex-wrap",
-        (0 until 20).map { i =>
-          <.div(
-            ^.key := s"$i",
-            ^.cls := "pe-3 pb-3",
-            ^.width := s"${100 / itemsPerRow}%",
-            AppComponent.Component(AppComponent.Props(App(s"My App $i")))
-          )
-        }.toVdomArray
+        ^.cls := "card",
+        <.div(
+          ^.cls := "card-body",
+          <.h5(^.cls := "card-title", props.app.title),
+          <.h6(^.cls := "card-subtitle mb-2 text-muted", "Card subtitle"),
+          <.p(^.cls := "card-text", "Description"),
+        )
       )
     }
   }
@@ -32,4 +28,5 @@ object MainComponent {
       .backend(new Backend(_))
       .render(_.backend.render)
       .build
+
 }
